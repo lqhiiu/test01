65d2a1e5f04222dd89179d66a05321d3e4a7e1a6
qihua <linqihua@aliyun.com>
2017-01-10 14:41:17 +0800

    v2017.01.10-0003 同步代码

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-echo "hello ";
-echo "来到中国 ";
-echo " ";
-echo "......";  
+<!DOCTYPE HTML>
+<html>
+ <head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+ <body>
+ <p>Life was like a box of chocolates, you never know what you're gonna get.（生命就像一盒巧克力，结果往往出人意料。）---《阿甘正传》 
+ </p>
+ <img src="/upload/course/000/000/009/58117bf7d1215384.jpg" width="200" border="5" align="center">
+ </body>
+</html>
