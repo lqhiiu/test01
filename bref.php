107ed6744b76bf3897d7bffda21d61b412c09f72
qihua <linqihua@aliyun.com>
2017-01-11 15:05:55 +0800

    v2017.01.11-0001 同步代码

---
+++ bref.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+     <a href = "http://www.baidu.com">baidu.com</a>
+     <br><br>
+	<a href = "http://www.ifeng.com" target = "_blank">凤凰网</a>
+	<br><br>
+	<a href="mailto:php@php.cn">有问题的话，请给我们发邮件</a>  
+	<br><br>
+	<a href="#">这是一个空链接</a>  
+    </body>
+</html>
