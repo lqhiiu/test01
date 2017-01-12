6bcf7ea2a8515014570b8129bda73f32c16e3a35
qihua <linqihua@aliyun.com>
2017-01-12 11:06:20 +0800

    v2017.01.12-0002 同步代码

---
+++ css02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>php.cn</title>
+    <style type="text/css">
+    h1{
+        color:red;
+        width:350px;
+        text-align:center;
+        font-family:微软雅黑;
+    }
+    p{
+         line-height:150%;
+         text-indent:28px;
+         font-family:楷体;
+    }
+    .box{
+        font-style:italic;
+        font-weight:bold;
+        text-decoration:underline;
+    }
+    </style>
+    </head>
+    <body>
+        <h1>习近平心中的互联网</h1>
+        <p>互联网是20世纪最伟大的发明，它正在将人类“<span class="box">一网打尽</span>”，人们在不知不觉中已经融入了互联网生态，互联网让世界变成了“鸡犬之声相闻”的地球村。</p>
+    </body>
+</html>
