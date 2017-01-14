aa316ac113275a7d9341edc90bf3c1ff32c464e6
qihua <linqihua@aliyun.com>
2017-01-14 15:14:34 +0800

    v2017.01.14-0001 同步代码

--- index.php
+++ index.php
@@ -1,93 +1,19 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>PHP中文网(php.cn)</title>
-</head>
-
-<style type="text/css">
-.error {
-	color: red;
-	font-weight: bold;
-}
-</style>
-<body>
- 
- <?php
-// 定义变量并默认设置为空值
-$name = $email = $gender = $comment = $website = "";
-$nameErr = $emailErr = $genderErr = $commentErr = $websiteErr = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "*名字不能为空";
-    } else {
-        $name = test_input($_POST["name"]);
-    }
-    
-    if (empty($_POST["email"])) {
-        $emailErr = "*邮箱不能为空";
-    } else {
-        $email = test_input($_POST["email"]);
-    }
-    
-    if (empty($_POST['website'])) {
-        $website = "";
-    } else {
-        $website = test_input($_POST["website"]);
-    }
-    
-    if (empty($_POST['comment'])) {
-        $comment = "";
-    } else {
-        $comment = test_input($_POST["comment"]);
-    }
-    
-    if (empty($_POST["gender"])) {
-        $genderErr = "*性别不能为空";
-    } else {
-        $gender = test_input($_POST["gender"]);
-    }
+<?php
+// 创建一个有异常处理的函数
+class Dog{
+    public $name;
+    public $age;
 }
 
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+$dog = new Dog();
+$a = array (1, 2, array ("a", "b", "c"));
+$dog->name = "PIG";
+$dog->age = 18;
+
+$jstr = json_encode($dog);
+echo $jstr;
+
+var_dump(json_decode($jstr));
+print_r(json_decode($jstr));
+var_export($a);
 ?>
- 
- <h2>PHP 表单验证实例</h2>
-	<form method="post"
-		action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		名字: <input type="text" name="name"> <span class="error"> <?php echo $nameErr?></span>
-		<br> <br> 
-		E-mail: <input type="text" name="email"> <span class="error"> <?php echo $emailErr?></span>
-		<br> <br> 
-		网址: <input type="text" name="website"> 
-		<br> <br> 
-		备注: <textarea name="comment" rows="5" cols="40"></textarea>
-		<br> <br> 
-		性别: <input type="radio" name="gender" value="female"
-			checked="checked">女 <input type="radio" name="gender" value="male">男
-			<span class="error"> <?php echo $genderErr?></span> 
-		<br> <br> 
-		<input type="submit" name="submit" value="提交">
-	</form>
- 
- <?php
-echo "<h2>您输入的内容是:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
- 
- </body>
---
+++ php_get.php
@@ -0,0 +1,5 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+echo "你的名字是：".$_REQUEST["name"]."<br>";
+echo "你的性别是：".$_REQUEST["sex"]."<br>";
+echo "你的年龄是：".$_REQUEST["age"]."<br>";
